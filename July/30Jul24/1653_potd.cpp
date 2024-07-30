59806b48fd077a70487cf6fa87057a7135c0f056
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-07-30 15:06:20 +0530

    add effective, ineffective ,optimal solutions

--- July/30Jul24/1653_potd.cpp
+++ July/30Jul24/1653_potd.cpp
@@ -1,29 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 class Solution {
 public:
     int minimumDeletions(string s) {
+        /*
+            3 Approaches
+            -------------
+            1) Stack -> Effective && not optimal
+                TC : O(N)
+                SC : O(N)
+            2) Two pointer -> Not Effective && optimal
+                TC : O(N)
+                SC : O(1)
+            3) Couting -> Effective && Optimal
+                TC : O(N)
+                SC : O(1)
+        */
+        
         // Effective
         // TC : O(N) && SC : O(N)
-        stack<char> Stack;
+        // stack<char> Stack;
+        // int n = s.size();
+        // int cnt = 0;
+        // for(int i = 0;i<n;i++){
+        //     if(Stack.empty()){
+        //         Stack.push(s[i]);
+        //     }else{
+        //         char topElement = Stack.top();
+        //         if(topElement == 'b' && s[i] == 'a'){
+        //             cnt++;
+        //             Stack.pop();
+        //         }else{
+        //             Stack.push(s[i]);
+        //         }
+        //     }
+        // }
+        // return cnt;
+        
+        // Optimal Solution and Effective...
+        int bCount = 0,res = 0;
         int n = s.size();
-        int cnt = 0;
         for(int i = 0;i<n;i++){
-            if(Stack.empty()){
-                Stack.push(s[i]);
-            }else{
-                char topElement = Stack.top();
-                if(topElement == 'b' && s[i] == 'a'){
-                    cnt++;
-                    Stack.pop();
-                }else{
-                    Stack.push(s[i]);
-                }
-            }
+            if(s[i] == 'b') bCount++;
+
+            else res = min(res + 1,bCount); // counting the 'ba' pairs 
         }
-        return cnt;
+        return res;
 
 
         // Using Stack is the effective solution of this problem since we able to get O(N) time and O(1) space through two pointers..
